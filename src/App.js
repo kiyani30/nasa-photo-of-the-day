72a8d3f4484e2d56bfeb50c4198cabf13fe0cc07
kiyani <bamba.kiyani@gmail.com>
2019-09-12 14:00:30 -0400

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
+import Asteroid from './components/planetes';
 
 function App() {
+  const [asteroids, setAsteroid] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(response  => {
+            console.log(response);
+            const { data } = response;
+            setAsteroid({ asteroids: data });
+            //console.log(object);
+        })
+        .catch(error => {
+            console.log("No data received", error);
+        })
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Asteroid data={asteroids}/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/planetes.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import AsteroidCard from './planetesContent';
+import axios from 'axios';
+
+export default function Asteroid() {
+    const [Asteroids, setAsteroid] = useState([]);
+
+
+useEffect(() => {
+    axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(response  => {
+        console.log(response);
+        const object = response.data;
+        setAsteroid(object);
+        console.log(object);
+    })
+    .catch(error => {
+        console.log("No data received", error);
+    })
+}, []);
+    return(
+        <div className="container">
+            <div className="entry">{
+             <AsteroidCard
+                hdurl={Asteroids.hdurl}
+                title={Asteroids.title}
+                explanation={Asteroids.explanation}
+                />
+            }</div>
+        </div>
+    )
+}
